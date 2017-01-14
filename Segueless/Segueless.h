e7345983acc0c864fa87f94211bc54de504e5b23
hiragram <y@yura.me>
2017-01-14 23:03:04 +0900

    Initial commit

---
+++ Segueless/Segueless.h
@@ -0,0 +1,19 @@
+//
+//  Segueless.h
+//  Segueless
+//
+//  Created by yuyahirayama on 2017/01/14.
+//  Copyright © 2017年 Yuya Hirayama. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Segueless.
+FOUNDATION_EXPORT double SeguelessVersionNumber;
+
+//! Project version string for Segueless.
+FOUNDATION_EXPORT const unsigned char SeguelessVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Segueless/PublicHeader.h>
+
+

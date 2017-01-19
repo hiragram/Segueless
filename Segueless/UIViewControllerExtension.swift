17cc50287f2d652460232600fb01e9680256c63e
Yuya Hirayama <hiragram@users.noreply.github.com>
2017-01-19 15:23:48 +0900

    Fix typo

--- Segueless/UIViewControllerExtension.swift
+++ Segueless/UIViewControllerExtension.swift
@@ -9,14 +9,14 @@
 import UIKit
 
 public extension UIViewController {
-  public func present<T: StoryboardInstantitable>(viewControllerTypeToPresent: T.Type, animated: Bool = true, completion: (() -> Void)? = nil, configuration: ((T) -> Void)?) where T: UIViewController {
+  public func present<T: StoryboardInstantiatable>(viewControllerTypeToPresent: T.Type, animated: Bool = true, completion: (() -> Void)? = nil, configuration: ((T) -> Void)?) where T: UIViewController {
     let vc = T.instantiateFromStoryboard()
     _ = vc.view
     configuration?(vc)
     present(vc, animated: animated, completion: completion)
   }
 
-  public func show<T: StoryboardInstantitable>(_ vcType: T.Type, sender: Any?, configuration: ((T) -> Void)?) where T: UIViewController {
+  public func show<T: StoryboardInstantiatable>(_ vcType: T.Type, sender: Any?, configuration: ((T) -> Void)?) where T: UIViewController {
     let vc = T.instantiateFromStoryboard()
     _ = vc.view
     configuration?(vc)
